d2fcff44b8a522d0a5b608cc6cec7fcba2f0ca81
Wes Payne <noblepayne@noblepayne.com>
2020-12-05 00:36:31 -0800

    Day 5 cleanup, thanks @mglen

--- src/noblepayne/advent2020/day5.clj
+++ src/noblepayne/advent2020/day5.clj
@@ -15,28 +15,16 @@
        (map process-pass)))
 
 ;;;;;;;; PART 1 ;;;;;;;;;;;;;;;;;;;;
-(defn bpart [{:keys [:lower :upper]} ltr]
-  (let [𝚫 (- upper lower)
-        ½𝚫 (-> 𝚫 (/ 2) Math/floor int)
-        new-lower (- upper ½𝚫)
-        new-upper (+ lower ½𝚫)]
-    (case ltr
-      \F {:lower lower :upper new-upper}
-      \L {:lower lower :upper new-upper}
-      \B {:lower new-lower :upper upper}
-      \R {:lower new-lower :upper upper})))
-
-(defn find-base [start input]
-  (->> input
-       (reduce bpart start)
-       :lower))
-
-(def find-row (partial find-base {:lower 0 :upper 127}))
-(def find-col (partial find-base {:lower 0 :upper 7}))
+(defn pass-to-int [pass]
+  (Integer/parseInt
+   (->> pass
+        (map {\F 0 \B 1 \L 0 \R 1})
+        (apply str))
+   2))
 
 (defn seat-id [{:keys [:rowpass :colpass]}]
-  (let [row (find-row rowpass)
-        col (find-col colpass)]
+  (let [row (pass-to-int rowpass)
+        col (pass-to-int colpass)]
     (+ col (* 8 row))))
 
 (defn solve-1 [data]
@@ -46,16 +34,13 @@
 
 ;;;;;;;; PART 2 ;;;;;;;;;;;;;;;;;;;;
 (defn solve-2 [data]
-  (->> data
-       (map seat-id)
-       sort
-       (partition 2 1) ;; e.g. ([123, 124], [124, 125])
-       (map reverse)
-       (filter #(not= 1 (apply - %)))
-       first
-       first
-       dec))
-
+  (let [ids (map seat-id data)
+        max_ (apply max ids)
+        min_ (apply min ids)
+        seat-range (range min_ (inc max_))]
+    (- (apply + seat-range)
+       (apply + ids))))
+    
 ;;;;;;;; REPL ;;;;;;;;;;;;;;;;;;;;;;
 (comment
   (def testdata (load-data "day5p1t1.txt"))
